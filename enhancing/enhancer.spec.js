0c2446b672e03f39696173df6938be87fc5bfc62
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-04 21:08:08 -0400

    Adding mock items, code for repair test.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,6 +1,22 @@
-const [succeed, fail, repair, get] = require('./enhancer.js');
+const {succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
 
+const mockItems = {
+ sword: {
+  name: "Masamune",
+  durability: 87,
+  enhancement: 17,
+ },
+ armor: {
+  name: "Naraku",
+  durability: 65,
+  enhancement: 5,
+ },
+};
+
 describe('repair sets durability to 100.', () => {
- const expected = repair()
+ const { sword, armor } = mockItems ;
+ it('Returns an item with durability restored to 100', () => {
+  expect(item.durability).toBe(100)
+ })
 })
