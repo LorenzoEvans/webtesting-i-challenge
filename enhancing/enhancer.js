44774fa15576ff381d72ab564aba983bd7d457b5
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-05 20:07:50 -0400

    Saving code, last branch got corrupted.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,53 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+ succeed,
+ fail,
+ repair,
+ get,
 };
 
 function succeed(item) {
-  return { ...item };
+while(typeof item === 'object'){
+ if(item.enhancement < 20){
+ return item.enhancement + 1 ;
+}
+}
+ return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+while(typeof item === 'object'){
+ if (item.enhancement < 15){
+  return item.durability - 5 ;
+
+ }
+ else if (item.enhancement === 16){
+  return item.durability - 10 ; ;
+ }
+ else if (item.enhancement > 16){
+  return item.enhancement - 1 ;
+ }
+}
+ return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+while(typeof item === 'object'){
+ if (item.durability !== 100){
+  return item.durability = 100
+ }
+}
+ return { ...item };
 }
 
 function get(item) {
-  return { ...item };
+while(typeof item === 'object'){
+if (item.enhancement === 0){
+ return item ;
+}
+else {
+ newname = `[+${item.enhancement}]` + ` ${item.name}` ;
+ return item.name = newname ;
+}
+}
+ return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,68 @@
-const enhancer = require('./enhancer.js');
+const {succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
+
+const mockItems = {
+ sword: {
+  name: "Masamune",
+  durability: 87,
+  enhancement: 17,
+ },
+ blade: {
+  name: "Tolstoy",
+  durability: 45,
+  enhancement: 1,
+ },
+ armor: {
+  name: "Naraku",
+  durability: 65,
+  enhancement: 5,
+ },
+ shield: {
+  name: "Guildsman",
+  durability: 54,
+  enhancement: 17,
+ },
+ helmet: {
+  name: "Satellite",
+  durability: 70,
+  enhancement: 16,
+ }
+};
+const { sword, armor, shield, helmet, blade } = mockItems ;
+
+
+describe('repair sets durability to 100.', () => {
+ it('Returns an item with durability restored to 100', () => {
+  expect((repair(sword))).toBe(100); //assertion
+  expect(sword.durability).not.toBeLessThan(100);
+ });
+});
+
+describe('succeed increases enhancements up to 20.', () => {
+ it('Returns an item with an enhancement increased by one.', () => {
+  const { enhancement } = armor ;
+  expect(succeed(armor)).toBe(enhancement + 1);
+  expect(succeed(armor)).toBeGreaterThan(enhancement);
+ })
+})
+
+describe('fail alters enhancement or durability accordingly.', () => {
+ const { enhancement, durability } = shield ;
+ const armorDur = armor.durability ;
+ const helmetDur = helmet.durability ;
+it('Alters item properties according to client specs.', () => { 
+ expect(fail(armor)).toBe(armorDur - 5)
+ expect(fail(armor)).toBeLessThan(61)
+ expect(fail(shield)).toBe(enhancement - 1)
+ expect(fail(shield)).toBeLessThan(17)
+ expect(fail(helmet)).toBe(helmetDur - 10)
+ })
+})
+
+describe('get alters item name according to enhancement value', () => {
+ const actual = get(blade) ;
+ const expected = "[+1] Tolstoy"
+ it('Changes names for items with enhancements higher than 0.', () => {
+  expect(actual).toBe(expected)
+ })
+})
